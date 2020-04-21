a657b9d3fa1b31fe7bb1e55a316e168481fd380b
juguemseok <kalkin2@naver.comm>
2020-04-21 14:20:09 +0900

    test 추가

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,8 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 import static org.junit.Assert.*;
@@ -15,7 +17,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Transactional
+@Transactional // default rollback
 
 public class MemberServiceTest {
 
@@ -26,13 +28,42 @@ public class MemberServiceTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    EntityManager em ;
 
+    //transaction 기본값이 rollback이므로 insert 문을 실행 하지 않음. rollback 하거나 flush 사용하면된다.
     @Test
+    //@Rollback(false)
     public void 회원가입()throws  Exception{
         Member member = new Member();
         member.setName("kim");
-
+    
         Long saveId = memberService.join(member);
+
+        //실제 디비 저장 insert
+        //em.flush();
         assertEquals(member,memberRepository.findOne(saveId));
     }
+
+
+    @Test (expected = IllegalStateException.class)
+    public void test유저_중복가입() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kalkin");
+
+        Member member2 = new Member();
+        member2.setName("kalkin");
+
+        memberService.join(member1);
+        // when
+        memberService.join(member2);
+
+
+     // then
+        fail("예외가 발생해야 한다.");
+     }
+
+
+
 }

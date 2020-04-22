ece704ba3fc3bb5bf2380f833f63b77efef1db7b
juguemseok <kalkin2@naver.comm>
2020-04-22 15:07:30 +0900

    주문 서비스 로직 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,7 @@ import java.util.List;
 @Entity
 @Table( name = "orders")
 @Getter@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id@GeneratedValue
@@ -54,6 +57,53 @@ public class Order {
     public void setDelivery(Delivery delivery){
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem ... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //비즈니스 로직
+    /**
+     * 주문취소
+     */
+    public void cancel(){
+        //배송중 상태
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송중인 상품은 취소가 불가능 합니다");
+        }
+        //취소 가능하도록 변경
+        this.setStatus(OrderStatus.CANCLE);
+
+        for(OrderItem orderItem : orderItems){
+            orderItem.cancel();
+        }
+
+    }
+
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice(){
+        int totalPrice = 0;
+//        for(OrderItem item : orderItems) {
+//            totalPrice += item.getOrderPrice();
+//        }
+        //lamda
+        totalPrice += orderItems.stream()
+                    .mapToInt(OrderItem::getOrderPrice)
+                    .sum();
+        return totalPrice;
     }
 
 
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,8 +11,8 @@ import javax.persistence.*;
 @Entity
 @Table(name ="order_item")
 @Getter@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
-
 
     @Id
     @GeneratedValue
@@ -34,4 +36,26 @@ public class OrderItem {
 
     //주문수량
     private int count;
+
+    //생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+
+    //재고 증가
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //전체 금액 계산 ( 가격 x 수량)
+    public int getTotalPrice(){
+        return getCount() * getOrderPrice();
+    }
 }
